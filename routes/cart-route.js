ee58e7d83bb1f8495ea6d6b22eeb9defd446cd71
didadadida93 <didadadida93@gmail.com>
2020-02-22 16:16:53 +0700

    update cart

--- routes/cart-route.js
+++ routes/cart-route.js
@@ -4,6 +4,7 @@ const { authenticate } = require('../midllewares/auth')
 
 router.get('/open', authenticate, CartController.getCartWithStatusOpen)
 router.get('/offered', authenticate, CartController.getCartWithStatusOffered)
+router.get('/user', authenticate, CartController.getAllCartByUser)
 
 router.get('/', CartController.getAllCarts)
 router.get('/:cartId', CartController.getCart)
