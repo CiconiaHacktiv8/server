b7bd46ce281f08254ebe018706c05e1937cd2f76
didadadida93 <didadadida93@gmail.com>
2020-02-24 09:14:47 +0700

    add limit size

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ mongoose
   })
 
 app.use(cors())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false, limit: 10e7 }))
+app.use(express.json({ limit: 10e7 }))
 
 app.use('/', route)
 
